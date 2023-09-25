921a2270429c662fad2782d1469afac6ea6c7dd9
Kartik Nair <bot2.0@pop-os.localdomain>
2023-09-25 18:34:36 +0530

    one part of the code stopped working

--- tasks/task6/scraper.py
+++ tasks/task6/scraper.py
@@ -16,14 +16,17 @@ def scrape_livescore():
     tag3 = doc.find_all('strong')  ##Score Value
     tag3_1 = doc.find_all('span')
     titletag=doc.find(class_="ds-inline-flex ds-items-start ds-leading-none !ds-inline")
+    T1=doc.find_all(class_='ds-text-tight-m ds-font-bold ds-capitalize ds-truncate')    
+    T2=doc.find(class_="ds-text-tight-m ds-font-bold ds-capitalize ds-truncate")
     if tags and tags1 and tags2:
         tag = tags[0]
         tag1 = tags1[0]
         tag2 = tags2[0]
+        print
         if tag3 and tag3_1:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             data = (
-                f"{titletag['title']}{tags1[1].text} {tag2.text}\n: {tag3[0].text}To {tag3[1].text}\n{tag.text}\n{tag3_1[1].text}{tag1.text}\n")
+                f"{ T1[0].text,'VS',T1[1].text,titletag['title']}{tags1[1].text} {tag2.text}\n: {tag3[0].text}To {tag3[1].text}\n{tag.text}\n{tag3_1[1].text}{tag1.text}\n")
             append_to_csv(timestamp, data)
             return data
         else:
